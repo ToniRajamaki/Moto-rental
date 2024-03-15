f491a7e0394d54b789ae5c0eb25cfff45a80677e
TONI <zingoo16@gmail.com>
2024-03-15 10:48:38 +0200

    added new bike for call to action section

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -4,86 +4,88 @@ import InventoryCard from './InventoryCard';
 
 const bikes = [
   {
-    model: 'Ninja ZX-6R',
+    model: 'Aprilia RS 660',
     gear: 'Manual',
-    img:"https://purepng.com/public/uploads/large/purepng.com-motorcyclemotorcyclemotorbikebikecycleracing-bike-1701527509998savsa.png",
+    img:"https://github.com/ToniRajamaki/moto-rental/blob/main/public/images/appriilia.png?raw=true",
     year: 2022,
-    engine: 600, // assuming 'cc' as unit
-    mileage: 15, // assuming 'km/l' as unit
+    engine: 660, // assuming 'cc' as unit
+    mileage: 20000, // assuming 'km/l' as unit
+    price: 129 // assuming 'per day' as unit
+  },
+  {
+    model: 'Vespa GTS 150',
+    gear: 'Automatic',
+    img:"https://raw.githubusercontent.com/ToniRajamaki/moto-rental/main/public/images/vespa.webp",
+    year: 2005,
+    engine: 150, // assuming 'cc' as unit
+    mileage: 6000, // assuming 'km/l' as unit
+    price: 49 // assuming 'per day' as unit
+  },
+  {
+    model: 'BMW G 310 R',
+    gear: 'Manual',
+    img:"https://github.com/ToniRajamaki/moto-rental/blob/main/public/images/bmw.png?raw=true",
+    year: 2024,
+    engine: 500, // assuming 'cc' as unit
+    mileage: 39000, // assuming 'km/l' as unit
+    price: 89 // assuming 'per day' as unit
+  },
+  {
+    model: 'Ducati Monster 821',
+    gear: 'Manual',
+    img:"https://github.com/ToniRajamaki/moto-rental/blob/main/public/images/ducati.png?raw=true",
+    year: 2019,
+    engine: 800, // assuming 'cc' as unit
+    mileage: 8000, // assuming 'km/l' as unit
+    price: 109 // assuming 'per day' as unit
+  },
+  {
+    model: 'Harley-Davidson Iron 883',
+    gear: 'Manual',
+    img:"https://raw.githubusercontent.com/ToniRajamaki/moto-rental/main/public/images/harley.webp",
+    year: 2009,
+    engine: 450, // assuming 'cc' as unit
+    mileage: 50000, // assuming 'km/l' as unit
     price: 99 // assuming 'per day' as unit
   },
   {
-    model: 'CBR500R',
+    model: 'Honda CB300R',
+    gear: 'Automatic',
+    img:"https://raw.githubusercontent.com/ToniRajamaki/moto-rental/main/public/images/honda.webp",
+    year: 1992,
+    engine: 125, // assuming 'cc' as unit
+    mileage: 15000, // assuming 'km/l' as unit
+    price: 39 // assuming 'per day' as unit
+  },
+
+{
+  model: 'Yamaha MT-07',
+  gear: 'Manual',
+  img:"https://raw.githubusercontent.com/ToniRajamaki/moto-rental/main/public/images/yamaha.webp",
+  year: 2018,
+  engine: 500, // assuming 'cc' as unit
+  mileage: 19000, // assuming 'km/l' as unit
+  price: 99 // assuming 'per day' as unit
+},
+  {
+    model: 'KTM 390 Duke',
     gear: 'Manual',
-    img:"https://purepng.com/public/uploads/large/purepng.com-motorcyclemotorcyclemotorbikebikecycleracing-bike-1701527509998savsa.png",
-    year: 2021,
-    engine: 471, // assuming 'cc' as unit
-    mileage: 18, // assuming 'km/l' as unit
-    price: 79 // assuming 'per day' as unit
+    img:"https://raw.githubusercontent.com/ToniRajamaki/moto-rental/main/public/images/ktm.webp",
+    year: 2023,
+    engine: 200, // assuming 'cc' as unit
+    mileage: 10000, // assuming 'km/l' as unit
+    price: 49 // assuming 'per day' as unit
   },
   {
-    model: 'MT-09',
+    model: 'Suzuki SV650X',
     gear: 'Manual',
-    img:"https://purepng.com/public/uploads/large/purepng.com-motorcyclemotorcyclemotorbikebikecycleracing-bike-1701527509998savsa.png",
+    img:"https://raw.githubusercontent.com/ToniRajamaki/moto-rental/main/public/images/suzuki.webp",
     year: 2020,
-    engine: 847, // assuming 'cc' as unit
-    mileage: 14, // assuming 'km/l' as unit
-    price: 89 // assuming 'per day' as unit
+    engine: 650, // assuming 'cc' as unit
+    mileage: 25000, // assuming 'km/l' as unit
+    price: 109 // assuming 'per day' as unit
   },
-  {
-    model: 'MT-09',
-    gear: 'Manual',
-    img:"https://purepng.com/public/uploads/large/purepng.com-motorcyclemotorcyclemotorbikebikecycleracing-bike-1701527509998savsa.png",
-    year: 2020,
-    engine: 847, // assuming 'cc' as unit
-    mileage: 14, // assuming 'km/l' as unit
-    price: 89 // assuming 'per day' as unit
-  },
-  {
-    model: 'MT-09',
-    gear: 'Manual',
-    img:"https://purepng.com/public/uploads/large/purepng.com-motorcyclemotorcyclemotorbikebikecycleracing-bike-1701527509998savsa.png",
-    year: 2020,
-    engine: 847, // assuming 'cc' as unit
-    mileage: 14, // assuming 'km/l' as unit
-    price: 89 // assuming 'per day' as unit
-  },
-  {
-    model: 'MT-09',
-    gear: 'Manual',
-    img:"https://purepng.com/public/uploads/large/purepng.com-motorcyclemotorcyclemotorbikebikecycleracing-bike-1701527509998savsa.png",
-    year: 2020,
-    engine: 847, // assuming 'cc' as unit
-    mileage: 14, // assuming 'km/l' as unit
-    price: 89 // assuming 'per day' as unit
-  },
-  {
-    model: 'MT-09',
-    gear: 'Manual',
-    img:"https://purepng.com/public/uploads/large/purepng.com-motorcyclemotorcyclemotorbikebikecycleracing-bike-1701527509998savsa.png",
-    year: 2020,
-    engine: 847, // assuming 'cc' as unit
-    mileage: 14, // assuming 'km/l' as unit
-    price: 89 // assuming 'per day' as unit
-  },
-  {
-    model: 'MT-09',
-    gear: 'Manual',
-    img:"https://purepng.com/public/uploads/large/purepng.com-motorcyclemotorcyclemotorbikebikecycleracing-bike-1701527509998savsa.png",
-    year: 2020,
-    engine: 847, // assuming 'cc' as unit
-    mileage: 14, // assuming 'km/l' as unit
-    price: 89 // assuming 'per day' as unit
-  },
-  {
-    model: 'GSX-S750',
-    gear: 'Manual',
-    img:"https://purepng.com/public/uploads/large/purepng.com-motorcyclemotorcyclemotorbikebikecycleracing-bike-1701527509998savsa.png",
-    year: 2019,
-    engine: 749, // assuming 'cc' as unit
-    mileage: 17, // assuming 'km/l' as unit
-    price: 85 // assuming 'per day' as unit
-  }
+ 
 ];
 
 function Inventory() {
