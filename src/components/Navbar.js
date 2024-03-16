620e781a2e9b11010ff57276e091ffca480e7d6e
TONI <zingoo16@gmail.com>
2024-03-16 14:46:08 +0200

    working on new logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,8 @@ const Navbar = () => {
 
   return (
     <nav >
-      <a href="/">
-        <img className='nav-logo'  src="https://raw.githubusercontent.com/ToniRajamaki/moto-rental/26ac057dbac091c9de1d1c2c0b374114d740da1d/public/images/low-poly-grid-haikei.svg" alt="" />
+      <a href="/" className='nav-title'>
+        <img className='nav-logo'  src="https://github.com/ToniRajamaki/moto-rental/blob/main/public/images/moto-rental-logo.png?raw=true" alt="" />Moto Rental
       </a>
       <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
         <span></span>
