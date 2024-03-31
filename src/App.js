35306d66c3a3fe99f461a5903be48d7aae2520ee
TONI <zingoo16@gmail.com>
2024-03-31 10:45:24 +0300

    added vehilces page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Home from './components/Home'
 import PageAbout from './components/PageAbout'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom' // Import Routes instead of Switch
+import PageVehicles from './components/PageVehicles'
 
 const baseURL = '/moto-rental'
 
@@ -15,6 +16,7 @@ function App() {
           {/* Use Routes instead of Switch */}
           <Route exact path='/' element={<Home />} />
           <Route path='/about-us' element={<PageAbout />} />
+          <Route path='/vehicles' element={<PageVehicles />} />
         </Routes>
       </div>
     </Router>
---
+++ src/components/PageVehicles.js
@@ -0,0 +1,45 @@
+import "../App.css"
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Search from '../components/Search'
+import Inventory from '../components/Inventory'
+import WhyUs from '../components/WhyUs'
+import Achievements from '../components/Achievements'
+import Testimonials from '../components/Testimonials'
+import Footer from '../components/Footer'
+import CallToActionCard from '../components/CallToActionCard'
+import SellingSection from "../components/SellingSection"
+import SpecialOffer from '../components/SpecialOffer'
+import ContactSection from "./ContactDivider"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const BR = () => {
+  return (
+    <div className='section-break'>
+      <br></br> <br></br>
+      <br></br> <br></br>
+    </div>
+  )
+}
+
+const baseURL = "/moto-rental";
+
+function Home() {
+  return (
+    <div className='App'>
+      <SpecialOffer />
+      <Navbar />
+      <BR />
+      <Search />
+      <BR />
+      <Inventory />
+      <BR />
+      <ContactSection />
+      <BR />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
